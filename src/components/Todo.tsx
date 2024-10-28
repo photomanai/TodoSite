2da5eaf5b6e0b0f1635bb11237e21032afca153a
photomanai <photom4n.41@gmail.com>
2024-10-28 23:24:51 +0400

    Todo update aded

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdOutlineBookmarkRemove, MdEdit } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 import { TodoType } from '../types/Types';
-import { useDispatch } from 'react-redux';
-import { removeTodoById } from '../redux/slices/TodoSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeTodoById, updateTodoById } from '../redux/slices/TodoSlice';
 
 interface TodoProps{
   todoProps: TodoType
@@ -13,16 +13,46 @@ function Todo({todoProps}:TodoProps) {
   const {id, content} = todoProps;
   const dispatch = useDispatch();
 
+  const [newTodoContent, setNewTodoContent] = useState<string>(content); 
+  const [editable , setEditable] = useState<boolean>(false);
+
   const handleRemoveTodo= ():void =>{
     dispatch(removeTodoById(id))
   }
 
+  const updateTodo = ():void =>{
+    const request:TodoType = {
+      id: id,
+      content : newTodoContent
+    }
+    dispatch(updateTodoById(request))
+    setEditable(false)
+  }
+
+
   return (
   <div className="todo-row">
-    <p>{content}</p>
+    <p>
+      {
+        editable?(
+          <input 
+          className="edit-todo-input"
+          value={newTodoContent}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+            setNewTodoContent(e.target.value)
+          }}
+          type="text" />
+        ):(content)  
+      }
+      </p>
     <div className="icon-c">
-        <FaCheck className="edit-icon"  /> 
-        <MdEdit className="edit-icon" />
+      {
+        editable?(
+        <FaCheck onClick={updateTodo} className="edit-icon"  /> 
+        ):(
+        <MdEdit  onClick={()=>{setEditable(true)}} className="edit-icon" />
+      )
+      }
       <MdOutlineBookmarkRemove onClick={handleRemoveTodo} className="remove-icon"  />
     </div>
   </div>
--- src/redux/slices/TodoSlice.tsx
+++ src/redux/slices/TodoSlice.tsx
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { Action, PayloadAction } from '@reduxjs/toolkit'
 import { TodoInitialState, TodoType } from '../../types/Types'
+import { act } from 'react'
 
 const initialState : TodoInitialState = {
     todos: [],
@@ -23,9 +24,21 @@ export const todoSlice = createSlice({
             state.todos = [...state.todos.filter((todo:TodoType)=>{
                 return todo.id !== action.payload
             })]
+        },
+        updateTodoById: (state: TodoInitialState , action:PayloadAction<TodoType>)=>{
+            // const filteredTodos = state.todos.filter((todo:TodoType)=>{
+            //     return todo.id !== action.payload.id
+            // })
+            const filteredTodos = state.todos.map((todo:TodoType)=>{
+                if (todo.id !== action.payload.id){
+                    return todo;
+                }
+                return action.payload
+            })
+            state.todos = [...filteredTodos]
         }
     }
 })
 
-export const { createTodo, removeTodoById } = todoSlice.actions
+export const { createTodo, removeTodoById, updateTodoById } = todoSlice.actions
 export default todoSlice.reducer
